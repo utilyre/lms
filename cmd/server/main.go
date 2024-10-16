8b9fbcd01f9dcf428754994fc07335a6a1bf1dc6
Amirabbas Fazelinia <utilyre@gmail.com>
2024-10-16 19:32:45 +0330

    feat(users): add GET /users/:id route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
@@ -38,8 +39,44 @@ func main() {
 
 	e := echo.New()
 
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/helloworld", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello world!")
+	})
+
+	e.GET("/users/:id", func(c echo.Context) error {
+		type Req struct {
+			ID int32 `param:"id"`
+		}
+
+		var req Req
+		if err := c.Bind(&req); err != nil {
+			return err
+		}
+
+		var user repository.User
+		if err := db.
+			NewSelect().
+			Model(&user).
+			Where("id = ?", req.ID).
+			Scan(c.Request().Context()); err != nil {
+			return err
+		}
+
+		type Resp struct {
+			ID    int32  `json:"id"`
+			Name  string `json:"name"`
+			Email string `json:"email"`
+			Role  string `json:"role"`
+		}
+		return c.JSON(http.StatusCreated, Resp{
+			ID:    user.ID,
+			Name:  user.Name,
+			Email: user.Email,
+			Role:  user.Role,
+		})
 	})
 
 	e.POST("/users", func(c echo.Context) error {
--- go.mod
+++ go.mod
@@ -7,9 +7,11 @@ require (
 	github.com/uptrace/bun v1.2.3
 	github.com/uptrace/bun/dialect/pgdialect v1.2.3
 	github.com/uptrace/bun/driver/pgdriver v1.2.3
+	golang.org/x/crypto v0.26.0
 )
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -20,9 +22,9 @@ require (
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	mellium.im/sasl v0.3.1 // indirect
 )
